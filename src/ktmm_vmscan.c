7b9138bbb594746d88c7c69fdbba2a3b300ded46
Sudarshan <159210386+ttsudarshan@users.noreply.github.com>
2025-10-22 14:34:29 -0500

    acccessed folio

--- src/ktmm_vmscan.c
+++ src/ktmm_vmscan.c
@@ -124,7 +124,7 @@ static bool ktmm_zone_watermark_ok_safe(struct zone *z,
 					unsigned long mark,
 					int highest_zoneidx)
 {
-  printk(KERN_INFO "sudarshan: entered %s\n", __func__);
+  //printk(KERN_INFO "sudarshan: entered %s\n", __func__);
 
 	return pt_zone_watermark_ok_safe(z, order, mark, highest_zoneidx);
 }
@@ -132,7 +132,7 @@ static bool ktmm_zone_watermark_ok_safe(struct zone *z,
 
 static struct pglist_data *ktmm_first_online_pgdat(void)
 {
-  printk(KERN_INFO "sudarshan: entered %s\n", __func__);
+  //printk(KERN_INFO "sudarshan: entered %s\n", __func__);
 
 	return pt_first_online_pgdat();
 }
@@ -140,7 +140,7 @@ static struct pglist_data *ktmm_first_online_pgdat(void)
 
 static struct zone *ktmm_next_zone(struct zone *zone)
 {
-  printk(KERN_INFO "sudarshan: entered %s\n", __func__);
+  //printk(KERN_INFO "sudarshan: entered %s\n", __func__);
 
 	return pt_next_zone(zone);
 }
@@ -148,7 +148,7 @@ static struct zone *ktmm_next_zone(struct zone *zone)
 
 static void ktmm_free_unref_page_list(struct list_head *list)
 {
-  printk(KERN_INFO "sudarshan: entered %s\n", __func__);
+  //printk(KERN_INFO "sudarshan: entered %s\n", __func__);
 
 	return pt_free_unref_page_list(list);
 }
@@ -156,7 +156,7 @@ static void ktmm_free_unref_page_list(struct list_head *list)
 
 static void ktmm_lru_add_drain(void)
 {
-  printk(KERN_INFO "sudarshan: entered %s\n", __func__);
+  //printk(KERN_INFO "sudarshan: entered %s\n", __func__);
 
 	pt_lru_add_drain();
 }
@@ -165,7 +165,7 @@ static void ktmm_lru_add_drain(void)
 static void ktmm_cgroup_update_lru_size(struct lruvec *lruvec, enum lru_list lru,
 					int zid, int nr_pages)
 {
-  printk(KERN_INFO "sudarshan: entered %s\n", __func__);
+  //printk(KERN_INFO "sudarshan: entered %s\n", __func__);
 
 	pt_cgroup_update_lru_size(lruvec, lru, zid, nr_pages);
 }
@@ -173,7 +173,7 @@ static void ktmm_cgroup_update_lru_size(struct lruvec *lruvec, enum lru_list lru
 
 static void ktmm_cgroup_uncharge_list(struct list_head *page_list)
 {
-  printk(KERN_INFO "sudarshan: entered %s\n", __func__);
+  //printk(KERN_INFO "sudarshan: entered %s\n", __func__);
 
 	pt_cgroup_uncharge_list(page_list);
 }
@@ -183,7 +183,7 @@ static unsigned long ktmm_isolate_lru_folios(unsigned long nr_to_scan, struct lr
 					struct list_head *dst, unsigned long *nr_scanned,
 					struct scan_control *sc, enum lru_list lru)
 {
-  printk(KERN_INFO "sudarshan: entered %s\n", __func__);
+  //printk(KERN_INFO "sudarshan: entered %s\n", __func__);
 
 	return pt_isolate_lru_folios(nr_to_scan, lruvec, dst, nr_scanned, sc, lru);
 }
@@ -191,7 +191,7 @@ static unsigned long ktmm_isolate_lru_folios(unsigned long nr_to_scan, struct lr
 
 static unsigned int ktmm_move_folios_to_lru(struct lruvec *lruvec, struct list_head *list)
 {
-  printk(KERN_INFO "sudarshan: entered %s\n", __func__);
+  //printk(KERN_INFO "sudarshan: entered %s\n", __func__);
 
 	return pt_move_folios_to_lru(lruvec, list);
 }
@@ -199,7 +199,7 @@ static unsigned int ktmm_move_folios_to_lru(struct lruvec *lruvec, struct list_h
 
 static void ktmm_folio_putback_lru(struct folio *folio)
 {
-  printk(KERN_INFO "sudarshan: entered %s\n", __func__);
+  //printk(KERN_INFO "sudarshan: entered %s\n", __func__);
 
 	pt_folio_putback_lru(folio);
 }
@@ -208,7 +208,7 @@ static void ktmm_folio_putback_lru(struct folio *folio)
 static int ktmm_folio_referenced(struct folio *folio, int is_locked,
 				struct mem_cgroup *memcg, unsigned long *vm_flags)
 {
-  printk(KERN_INFO "sudarshan: entered %s\n", __func__);
+  //printk(KERN_INFO "sudarshan: entered %s\n", __func__);
 
 	return pt_folio_referenced(folio, is_locked, memcg, vm_flags);
 }
@@ -319,7 +319,7 @@ static int track_folio_access(struct folio *folio, struct pglist_data *pgdat, co
  */
 struct page* alloc_pmem_page(struct  page *page, unsigned long data)
 {
-  printk(KERN_INFO "sudarshan: entered %s\n", __func__);
+  //printk(KERN_INFO "sudarshan: entered %s\n", __func__);
 	gfp_t gfp_mask = GFP_USER | __GFP_PMEM;
 	return alloc_page(gfp_mask);
 }
@@ -345,7 +345,7 @@ static struct page *ktmm_alloc_pages(gfp_t gfp_mask, unsigned int order, int pre
 {
 	//node mask of pmem_node
 	//pass node mask into alloc pages
-  printk(KERN_INFO "sudarshan: entered %s\n", __func__);
+  //printk(KERN_INFO "sudarshan: entered %s\n", __func__);
 
 	nodemask_t nodemask_test;
 	int nid;
@@ -389,7 +389,7 @@ static struct page *ktmm_alloc_pages(gfp_t gfp_mask, unsigned int order, int pre
  */
 static bool ktmm_cgroup_below_low(struct mem_cgroup *memcg)
 {
-  printk(KERN_INFO "sudarshan: entered %s\n", __func__);
+  //printk(KERN_INFO "sudarshan: entered %s\n", __func__);
 
 	return READ_ONCE(memcg->memory.elow) >=
 		page_counter_read(&memcg->memory);
@@ -405,7 +405,7 @@ static bool ktmm_cgroup_below_low(struct mem_cgroup *memcg)
  */
 static bool ktmm_cgroup_below_min(struct mem_cgroup *memcg)
 {
-  printk(KERN_INFO "sudarshan: entered %s\n", __func__);
+  //printk(KERN_INFO "sudarshan: entered %s\n", __func__);
 
 	return READ_ONCE(memcg->memory.emin) >=
 		page_counter_read(&memcg->memory);
@@ -424,7 +424,7 @@ static bool ktmm_cgroup_below_min(struct mem_cgroup *memcg)
 static __always_inline void ktmm_update_lru_sizes(struct lruvec *lruvec,
 			enum lru_list lru, unsigned long *nr_zone_taken)
 {
-  printk(KERN_INFO "sudarshan: entered %s\n", __func__);
+  //printk(KERN_INFO "sudarshan: entered %s\n", __func__);
 
 	int zid;
 
@@ -447,7 +447,7 @@ static __always_inline void ktmm_update_lru_sizes(struct lruvec *lruvec,
  */
 static inline bool ktmm_folio_evictable(struct folio *folio)
 {
-  printk(KERN_INFO "sudarshan: entered %s\n", __func__);
+  //printk(KERN_INFO "sudarshan: entered %s\n", __func__);
 
 	bool ret;
 
@@ -468,7 +468,7 @@ static inline bool ktmm_folio_evictable(struct folio *folio)
  */
 static inline bool ktmm_folio_needs_release(struct folio *folio)
 {
-  printk(KERN_INFO "sudarshan: entered %s\n", __func__);
+  //printk(KERN_INFO "sudarshan: entered %s\n", __func__);
 
 	struct address_space *mapping = folio_mapping(folio);
 
@@ -495,7 +495,7 @@ static void scan_promote_list(unsigned long nr_to_scan,
 				enum lru_list lru,
 				struct pglist_data *pgdat)
 {
-  printk(KERN_INFO "sudarshan: entered %s\n", __func__);
+  //printk(KERN_INFO "sudarshan: entered %s\n", __func__);
 
 	unsigned long nr_taken;
 	unsigned long nr_scanned;
@@ -577,7 +577,7 @@ static void scan_active_list(unsigned long nr_to_scan,
 				enum lru_list lru,
 				struct pglist_data *pgdat)
 {
-  printk(KERN_INFO "sudarshan: entered %s\n", __func__);
+  //printk(KERN_INFO "sudarshan: entered %s\n", __func__);
 
 	unsigned long nr_taken;
 	unsigned long nr_scanned;
@@ -718,7 +718,7 @@ static unsigned long scan_inactive_list(unsigned long nr_to_scan,
 					enum lru_list lru,
 					struct pglist_data *pgdat)
 {
-  printk(KERN_INFO "sudarshan: entered %s\n", __func__);
+  //printk(KERN_INFO "sudarshan: entered %s\n", __func__);
 
 	LIST_HEAD(folio_list);
 	unsigned long nr_scanned;
@@ -799,7 +799,7 @@ static unsigned long scan_list(enum lru_list lru,
 				struct scan_control *sc,
 				struct pglist_data *pgdat)
 {
-  printk(KERN_INFO "sudarshan: entered %s\n", __func__);
+  //printk(KERN_INFO "sudarshan: entered %s\n", __func__);
 
 	if (is_active_lru(lru))
 		scan_active_list(nr_to_scan, lruvec, sc, lru, pgdat);
@@ -824,7 +824,7 @@ static void scan_node(pg_data_t *pgdat,
 		struct scan_control *sc,
 		struct mem_cgroup_reclaim_cookie *reclaim)
 {
-  printk(KERN_INFO "sudarshan: entered %s\n", __func__);
+  //printk(KERN_INFO "sudarshan: entered %s\n", __func__);
 
 	enum lru_list lru;
 	struct mem_cgroup *memcg;
@@ -891,7 +891,7 @@ static void scan_node(pg_data_t *pgdat,
  */
 static void tmemd_try_to_sleep(pg_data_t *pgdat, int nid)
 {
-  printk(KERN_INFO "sudarshan: entered %s\n", __func__);
+  //printk(KERN_INFO "sudarshan: entered %s\n", __func__);
 
 	long remaining = 0;
 	DEFINE_WAIT(wait);
@@ -917,7 +917,7 @@ static void tmemd_try_to_sleep(pg_data_t *pgdat, int nid)
  */
 static int tmemd(void *p) 
 {
-  printk(KERN_INFO "sudarshan: entered %s\n", __func__);
+  //printk(KERN_INFO "sudarshan: entered %s\n", __func__);
 
 	pg_data_t *pgdat = (pg_data_t *)p;
 	int nid = pgdat->node_id;
