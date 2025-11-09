573191b582d5a91c8168f91f614c14877a23eb54
Sudarshan <159210386+ttsudarshan@users.noreply.github.com>
2025-11-08 18:37:44 -0600

    changes lru list to 2 million

--- src/ktmm_vmscan.c
+++ src/ktmm_vmscan.c
@@ -835,7 +835,7 @@ static void scan_node(pg_data_t *pgdat,
 		scanned = sc->nr_scanned;
 
 		for_each_evictable_lru(lru) {
-			unsigned long nr_to_scan = 256;  //sudarshan changed this to 256 for better page access detection
+			unsigned long nr_to_scan = 2000000;  //sudarshan changed this to 256 for better page access detection
 
 			scan_list(lru, nr_to_scan, lruvec, sc, pgdat);
 			
